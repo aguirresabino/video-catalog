42317e431abb4def0646001ec8a27b75c5209616
Aguirre Sabino <aguirresabino187@gmail.com>
2021-05-31 00:29:39 -0300

    feat: implements category resource

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'App\Http\Api'], function() {
+    Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
+});
